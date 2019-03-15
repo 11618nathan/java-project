a613681efcefb740627ae9490cf2c22550d0ec6f
Baek Gi Chan <11618nathan@gmail.com>
2019-03-15 14:34:01 +0900

    Upload

---
+++ 2조 은행계좌관리프로그램/src/BankMain.java
@@ -0,0 +1,13 @@
+
+public class BankMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub\
+		
+		Account a1 = new Account();
+		a1.showBalance();
+		a1.deposit(10000);
+		
+		System.out.println();
+	}
+}
